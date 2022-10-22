4ca344b6947200c392f52eeb920ca96c61e88133
Eduardo Rodrigues <silva.rodriguesedu@icloud.com>
2022-10-22 10:10:50 -0300

    Estruturação do projeto

---
+++ react-frontend/src/components/Mapping.tsx
@@ -0,0 +1,11 @@
+import {Grid} from "@material-ui/core";
+
+type Props = {
+    
+};
+export const Mapping = (props: Props) => {
+    return <Grid container>
+        <Grid item>Formulário</Grid>
+        <Grid item>Mapa</Grid>
+    </Grid>
+};
